08192d48b0b24b305321f302854ffe308c9d8b82
tho2011 <tho2011@my.fit.edu>
2022-05-27 19:09:37 -0400

    Created setup script for repo

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/tho2011/wordle_player.git
+
+conda create -n wordle python=3.8 numpy pandas scipy tqdm
+source activate wordle || conda activate wordle
+
+cd wordle
+pip install -e .
+
+echo "Setup completed!"
