df619d2513aaf2b94c9592ab662f50e14c227b41
tho2011 <tho2011@my.fit.edu>
2022-05-27 23:15:58 -0400

    Added green and yellow coloring logic

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,5 +1,7 @@
-class Wordle:
+from colorama import Fore, Style
 
+
+class Wordle:
     def __init__(self, target_word, accepted_words):
         assert target_word in accepted_words
         self.guesses = []
@@ -12,13 +14,30 @@ class Wordle:
         print()
 
     def add_guess(self, guess):
-        self.guesses.append(guess)
+        counts = {}
+        colors = [Fore.WHITE] * 5
 
+        target_rem = []
+        guess_rem = []
 
+        for i, ch in enumerate(guess):
+            if ch == self.target_word[i]:
+                colors[i] = Fore.GREEN
+            else:
+                target_rem.append(self.target_word[i])
+                guess_rem.append((ch, i))
 
+        for s in guess_rem:
+            if s[0] in target_rem:
+                colors[s[1]] = Fore.YELLOW
+                target_rem.remove(s[0])
+
+        self.guesses.append(
+            f"{colors[0]}{guess[0]}{colors[1]}{guess[1]}{colors[2]}{guess[2]}{colors[3]}{guess[3]}{colors[4]}{guess[4]}{Style.RESET_ALL}"
+        )
 
     def play(self):
-        while(True):
+        while True:
             self.show()
             guess = input(f"Input guess {len(self.guesses) + 1}: ")
             print()
@@ -44,12 +63,22 @@ class Wordle:
         self.show()
         print("Game Over")
 
-def main():
-    valid_list = ['acids','belts','adieu','adore','nests','fresh','pizza']
-    game = Wordle('belts',valid_list)
 
+def main():
+    valid_list = [
+        "acids",
+        "belts",
+        "adieu",
+        "adore",
+        "nests",
+        "fresh",
+        "pizza",
+        "robot",
+        "rooto",
+    ]
+    game = Wordle("robot", valid_list)
     game.play()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
