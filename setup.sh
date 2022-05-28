dae90d761f63259622bebcecaf41f55d6b11b4e9
tho2011 <tho2011@my.fit.edu>
2022-05-28 04:17:00 -0400

    Added dependencies for notebooks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/tho2011/wordle_player.git
 
-conda create -n wordle python=3.8 numpy pandas scipy tqdm colorama
+conda create -n wordle python=3.8 numpy pandas scipy tqdm colorama matplotlib ipykernel jupyterlab
 source activate wordle || conda activate wordle
 
 cd wordle
