0149cb6680ad2dbe76c8a4d1b23e577efca387f2
tho2011 <tho2011@my.fit.edu>
2022-05-28 18:17:57 -0400

    fixed file reading

--- wordle/play_wordle.py
+++ wordle/play_wordle.py
@@ -7,14 +7,25 @@ def main():
     valid_words = "valid_wordle_words.txt"
     targets = "us_solutions.txt"
 
+    us_list = []
+    us_soln = []
+
+
     with open(valid_words, "r") as file:
-         us_list = [word.strip().replace('"','') for word in file.read().split(',')]
+        for line in file:
+            for word in line.split(','):
+                us_list.append(word.replace('"','').strip())
 
     with open(targets, "r") as file:
-         us_soln = [word.strip().replace('"','') for word in file.read().split(',')]
+        for line in file:
+            for word in line.split(','):
+                us_soln.append(word.replace('"','').strip())
+
+    # print(len(us_list))
+    # print(len(us_soln))
 
     us_soln.sort()
-
+    #
     game = Wordle(random.choice(us_soln), us_list+us_soln)
     game.play()
 
